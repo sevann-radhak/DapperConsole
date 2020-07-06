8a468767a44b07b1cf5abed001a0abbe51cb60be
Sevann Radhak <sevann.radhak@gmail.com>
2020-07-06 19:17:56 -0300

    First commit. Bassic dapper querys

---
+++ DapperConsole/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperConsole
+{
+    public class Author
+    {
+        public int author_id { get; set; }
+        public string first_name { get; set; }
+        public string phone { get; set; }
+    }
+}
---
+++ DapperConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DapperConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = "Server=LAPTOP-KOQ9VHI4;Database=BookStoresDB;Trusted_Connection=True;";
+
+            using (var db = new SqlConnection(connection))
+            {
+                var sqlInsert = $"INSERT INTO Author(last_name, first_name, phone) VALUES (@last_name, @first_name, @phone)";
+                var restul = db.Execute(sqlInsert,
+                    new
+                    {
+                        last_name = "Radhak",
+                        first_name = "Sevann",
+                        phone = "1173627795"
+                    });
+
+                var sql = "SELECT author_id, first_name, phone FROM Author";
+                var list = db.Query<Author>(sql);
+
+                //foreach (var author in list)
+                //{
+                //    Console.WriteLine(author.first_name);
+                //}
+
+                list.Select(a => { Console.WriteLine($"{a.author_id} | {a.first_name} {a.phone}"); return a; }).ToList();
+            }
+        }
+    }
+}
